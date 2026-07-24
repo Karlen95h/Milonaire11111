d937e769e330f50ca4bc295615ee4a68be333e55
Karlen95h <karlen.1995.h@gmail.com>
2026-07-24 20:11:34 +0400

    css and js chages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-'use strict'
+'use strict';
 
-let   mainGame = document.querySelector('.game-block'),
+let mainGame = document.querySelector('.game-block'),
 gameWrapper = document.querySelector('.game-wrap'),
 startBtn = document.querySelector('.start-btn'),
 endBtn = document.querySelector('.end-btn'),
-btnAnswers = document.querySelector('.answer'),
-blockQuestion = document.querySelector('.question'),
-helpBtns = document.querySelector('.hints-help'),
-winBlock = document.querySelector('.wins-block')
+btnAnswers = document.querySelectorAll('.answer'),
+blockQuestion  = document.querySelectorAll('.question'),
+helpBtns = document.querySelectorAll('.hints-help'),
+winBlock = document.querySelectorAll('.wins-block'),
+helpFifty = document.querySelector('.fifty-fifty'),
+helpHall  = document.querySelector('.hall-help'),
+helpFriend = document.querySelector('.call-friend'),
+helpAI = document.querySelector('.ai-help'),
+game = document.querySelector('.game')
+/////////===========>
+
+let changeQuestion = document.getElementById('hintBox')
+let extraQuestion = document.getElementById('extra')
+let flagExtra = true 
+
+
